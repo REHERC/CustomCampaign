f808e84fcc8dac08093aba771acb937d34a42432
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-10-03 14:35:30 +0200

    Create Entry.cs

---
+++ CustomCampaign/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+
+namespace CustomCampaign
+{
+    public class Entry : IPlugin
+    {
+        public void Initialize(IManager manager, string ipcIdentifier)
+        {
+            
+        }
+    }
+}
