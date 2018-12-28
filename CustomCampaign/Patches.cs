c6cdee6ffe2cd1d8d5791acc1112213b4b53cb19
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-12-28 03:06:25 +0100

    namespace Array.[REDACTED]

--- CustomCampaign/Patches.cs
+++ CustomCampaign/Patches.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using Harmony;
 using Serializers;
-using Spectrum.API.Storage;
 
 namespace CustomCampaign
 {
@@ -13,48 +10,31 @@ namespace CustomCampaign
     {
         static void Postfix(LevelGridMenu __instance)
         {
-            bool flag_campaignmode = __instance.GetPrivateField<bool>("isCampaignMode_");
+            bool flag_campaignmode = __instance.GetPrivateField<bool>("isCampaignMode_") | true;
 
-            FileSystem fs = new FileSystem();
-            string path = $@"{fs.RootDirectory}/Data/Campaigns/";
+            GameModeID mode = GameModeID.LostToEchoes;
+            LevelType context = LevelType.Official;
+            LevelGridMenu.PlaylistEntry.UnlockStyle unlock = LevelGridMenu.PlaylistEntry.UnlockStyle.None;
 
-
-            if (flag_campaignmode)
+            LevelPlaylist playlist = new LevelPlaylist
             {
-                foreach (string campaignroot in Directory.GetDirectories(path))
-                {
-                    Console.WriteLine("folder : " + campaignroot);
-                    Campaign c = new Campaign();
-                    string pakfile = Path.GetFullPath(campaignroot + "/Campaign.pak");
-                    Console.WriteLine(pakfile);
-                    Console.WriteLine("exists : " + File.Exists(pakfile));
-                    Console.WriteLine("validated : " + c.Validate(pakfile));
-                    if (File.Exists(pakfile)
-                    && c.Validate(pakfile))
-                    {
-                        c.Load(pakfile);
-                        
-                        Console.WriteLine("campaign : " + c.Name);
-                        LevelSet set = new LevelSet();
+                Name_ = "Custom Campaign"
+            };
 
-                        foreach (Campaign.Level level in c.Levels)
-                        {
-                            string levelpath = Path.GetFullPath(campaignroot + Path.DirectorySeparatorChar + level.file);
-                            CampaignLevelInfo info = SharedResources.LevelFilesAndInfo[levelpath];
-                            Console.WriteLine("adding level to set : " + levelpath);
-                            set.AddLevel(info.name, levelpath, LevelType.Official);
-                        }
 
-                        GameModeID mode = GameModeID.Nexus;
-                        LevelGridMenu.PlaylistEntry.UnlockStyle unlock = LevelGridMenu.PlaylistEntry.UnlockStyle.PreviousLevels;
+            playlist.Add(new LevelPlaylist.ModeAndLevelInfo(mode, "Terminus", "OfficialLevels/Terminus.bytes"));
+            playlist.Add(new LevelPlaylist.ModeAndLevelInfo(mode, "Terminus", "OfficialLevels/01.bytes"));
+            playlist.Add(new LevelPlaylist.ModeAndLevelInfo(mode, "Factory Reset", "MyLevels/factory reset.bytes"));
+
+            LevelSet set = new LevelSet();
+            foreach (var level in playlist.GetLevelSet())
+                set.AddLevel(level.levelName_, level.levelPath_, context);
                         
-                        __instance.CallPrivateMethod("CreateAndAddCampaignLevelSet", set,  c.Name, true, unlock, mode);
-                        __instance.buttonList_.SortAndUpdateVisibleButtons();
-                    }
-                }
-            }
+            __instance.CallPrivateMethod("CreateAndAddCampaignLevelSet", set, playlist.Name_, true, unlock, mode);
+            __instance.buttonList_.SortAndUpdateVisibleButtons();
         }
     }
+
 
     [HarmonyPatch(typeof(LevelSetsManager))]
     [HarmonyPatch("SetupLevelPaths")]
@@ -62,52 +42,7 @@ namespace CustomCampaign
     {
         static void Postfix(LevelSetsManager __instance)
         {
-            FileSystem fs = new FileSystem();
-            string path = $@"{fs.RootDirectory}/Data/Campaigns/";
-
-            foreach (string campaignroot in Directory.GetDirectories(path))
-            {
-                Campaign c = new Campaign();
-                string pakfile = Path.GetFullPath(campaignroot + "/Campaign.pak");
-                if (File.Exists(pakfile)
-                && c.Validate(pakfile))
-                {
-                    c.Load(pakfile);
-                    foreach (Campaign.Level level in c.Levels)
-                    {
-                        string levelpath = Path.GetFullPath(campaignroot + Path.DirectorySeparatorChar + level.file);
-                        
-                        LevelNameAndPathPair levelpathpair = new LevelNameAndPathPair(level.levelname.GetForwardPath(), levelpath);
-                        
-                        LevelSettings levelsettings = LevelSettings.CreateAndLoadFromPath(levelpath.GetForwardPath(), out bool settingsvalidlevelpath);
-                        
-                        LevelInfo levelinfo = LevelInfo.Create(levelpath.GetForwardPath(), levelsettings);
-                        levelinfo.fileNameWithoutExtension_ = /*"test" + new System.Random().Next(int.MinValue,int.MaxValue);*/levelpath.GetForwardPath();
-                        levelinfo.levelType_ = LevelType.Official;
-
-                        Console.WriteLine("adding level to internal registry : " + levelpath);
-                        Console.WriteLine("invalid file path : " + settingsvalidlevelpath);
-
-                        Console.WriteLine("levelsettings : " + levelsettings is null ? "null" : "instantiated");
-                        Console.WriteLine("levelinfo : " + levelinfo is null ? "null" : "instantiated");
-
-                        UnityEngine.Debug.LogError("Level : " + levelsettings.levelName_);
-                        UnityEngine.Debug.LogError("        " + levelinfo.fileNameWithoutExtension_);
-                        UnityEngine.Debug.LogError("        " + levelpathpair.levelPath_);
-
-                        //__instance.AllLevelPaths_.Add(levelpath);
-                        //__instance.AllLevelsLevelNameAndPathPairs_.Add(levelpathpair);
-
-                        //__instance.OfficialLevelFilePaths_.Add(levelpath);
-                        //__instance.OfficialLevelInfosList_.Add(levelinfo);
-                        //__instance.OfficialLevelNameAndPathPairs_.Add(levelpathpair);
-
-                        __instance.AddOrUpdateLevelUsingLevelInfo(levelpath.GetForwardPath().Normalize(), levelinfo, false);
-
-                        SharedResources.LevelFilesAndInfo.Add(levelpath, new CampaignLevelInfo(level.levelname, level.levelname_sub, level.loading_wallpaper));
-                    }
-                }
-            }
+            
         }
     }
 
