c786dc8df25aa530ffadca2558b035b468fb8b2a
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-12-29 02:54:35 +0100

    Modified the TO DO list

--- CustomCampaign/TODO.cs
+++ CustomCampaign/TODO.cs
@@ -1,3 +1,3 @@
 ﻿//TODO: dig into LevelSetsManager >> this.officalLevelInfos_.LevelPathsToLevelInfos_; seems to be related to levelinfos (which causes problems)
 //TODO: use GetPrivateField to get access to the class member
-
+//TODO: figure out the difference between LevelInfo and LevelInfos (classes)
