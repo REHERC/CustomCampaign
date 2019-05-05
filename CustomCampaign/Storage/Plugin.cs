d137d82a73486f164ec63eb4f4efbdfbbaf84f0d
REHERC <amaury.vanoorenberghe@yahoo.com>
2019-05-05 16:15:40 +0200

    Fixed missing code in Plugin.LoadCampaigns

--- CustomCampaign/Storage/Plugin.cs
+++ CustomCampaign/Storage/Plugin.cs
@@ -30,27 +30,23 @@ namespace CustomCampaign
                 {
                     Campaign campaign_data = Campaign.FromFile(campaign_pak);
                     CampaignInfo campaign_info = new CampaignInfo(campaign_directory, extract_root, campaign_data);
-
                     foreach (Campaign.Level level in campaign_data.levels)
                     {
                         string level_filename = new List<string>(level.file.NormPath().Split('/')).Last().LowerCase();
                         string data_filename = $"{campaign_directory}/{level.file}".NormPath();
                         string docs_filename = $"{campaign_info.GetDocsPath()}/{level_filename}".NormPath();
-
                         Extensions.MakeDirectory(campaign_info.GetDocsPath());
-
                         Extensions.CopyFile(data_filename, docs_filename);
                         Extensions.CopyFile(data_filename + ".png", docs_filename + ".png");
-
                         Campaign.Level level_info = new Campaign.Level(
                             docs_filename,
                             level.levelname,
                             level.levelname_sub,
-                            $"{campaign_directory}/{level.loading_wallpaper}".NormPath()
+                            $"{campaign_directory}/{level.loading_wallpaper}".NormPath(),
+                            level.loading_text
                         );
                         campaign_info.Levels.Add(level_info);
                     }
-
                     foreach (string addon in campaign_data.addons)
                     {
                         AddonInfo addon_info = AddonInfo.Load(campaign_info, addon);
