625dc2ac63218d004a93045906c79c4be4449e3e
REHERC <amaury.vanoorenberghe@yahoo.com>
2019-09-21 00:02:57 +0200

    Changed the arcade campaign button color

--- CustomCampaign/Storage/Constants.cs
+++ CustomCampaign/Storage/Constants.cs
@@ -7,7 +7,7 @@
             internal const string LevelLocked_Message = "Complete previous levels to unlock.";
             internal const string LevelLocked_Title = "LEVEL LOCKED";
 
-            internal const string CampaignEntry_Color = "[F0CD4F]";
+            internal const string CampaignEntry_Color = "[EBC715]";
         }
     }
 }
