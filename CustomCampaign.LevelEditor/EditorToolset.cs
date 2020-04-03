8e8db5018f49f1d1233d76910fc3d4ea8eda5a44
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-04-03 19:07:49 +0200

    Moved group stack browsing to REHERC/EditorAdditions

--- CustomCampaign.LevelEditor/EditorToolset.cs
+++ CustomCampaign.LevelEditor/EditorToolset.cs
@@ -10,40 +10,5 @@ namespace CustomCampaign.LevelEditor
         public static void Initialize()
         {
         }
-
-        public static void SetQuickMemory(int index, GameObject instance)
-        {
-            QuickSelectMemory[index] = instance;
-        }
-
-        public static GameObject GetQuickMemory(int index)
-        {
-            if (QuickSelectMemory.ContainsKey(index))
-            {
-                return QuickSelectMemory[index];
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public static void Inspect(GameObject Target)
-        {
-            NGUIObjectInspectorTab Inspector = Object.FindObjectOfType<NGUIObjectInspectorTab>();
-
-            var Editor = G.Sys.LevelEditor_;
-
-            if (Editor && Inspector)
-            {
-                Editor.activeObject_ = Target;
-                Inspector.targetObject_ = Target;
-                Inspector.ClearComponentInspectors();
-                Inspector.InitComponentInspectorsOnTargetObject();
-                Inspector.InitAddComponentButton();
-                Inspector.propertiesNeedToBeUpdated_ = false;
-                Inspector.objectNameLabel_.text = Inspector.targetObject_.GetDisplayName();
-            }
-        }
     }
 }
