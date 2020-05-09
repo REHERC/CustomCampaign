8e9cc9125919b3ffafbbeb34b327854688204516
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-05-09 03:02:11 +0200

    Working component window close button :^)

--- CustomCampaign.Common/T4/Build Properties/Version.cs
+++ CustomCampaign.Common/T4/Build Properties/Version.cs
@@ -2,6 +2,6 @@
 {
 	public static class BuildProperties
 	{
-		public static ulong BuildNumber => 20200508025613;
+		public static ulong BuildNumber => 20200509025712;
 	}
 }
--- CustomCampaign.LevelEditor/Tools/Others/AddCustomComponent/AddCustomComponentMenuLogic.cs
+++ CustomCampaign.LevelEditor/Tools/Others/AddCustomComponent/AddCustomComponentMenuLogic.cs
@@ -16,19 +16,12 @@ namespace CustomCampaign.LevelEditor.Tools.Others
 
         private void OnEnable()
         {
-            //Plugin.Log.Info("OnEnable");
-
-            //Plugin.Log.Info("headerHierarchy");
-            //buttonList_ = GetComponentInChildren<UIExButtonList>();
-            //const string headerHierarchy = "Panel/Window Content/Header/Label";
-            //headerLabel_ = transform.Find(headerHierarchy).GetComponent<UILabel>();
-
-            //Plugin.Log.Info("inputHierarchy");
-            //const string inputHierarchy = "Panel/InputField";
-            //levelPathInput_ = transform.Find(inputHierarchy).GetComponent<UIExInput>();
-
-            //Plugin.Log.Info("GenerateComponentNameList");
             GenerateComponentNameList();
+        }
+
+        public void CloseWindow()
+        {
+            G.Sys.MenuPanelManager_.Pop(true);
         }
 
         internal void Init(LevelEditorLevelNameSelectMenuLogic menuLogic)
@@ -36,26 +29,53 @@ namespace CustomCampaign.LevelEditor.Tools.Others
             levelPathInput_ = menuLogic.levelPathInput_;
             headerLabel_ = menuLogic.headerLabel_;
             buttonList_ = menuLogic.buttonList_;
+
+            const string okButtonHierarchy = "Panel/OK Button/Sprite";
+            UIEventListener okButton = transform.Find(okButtonHierarchy).GetComponent<UIEventListener>();
+
+            const string cancelButtonHierarchy = "Panel/Cancel Button/Sprite";
+            UIEventListener cancelButton = transform.Find(cancelButtonHierarchy).GetComponent<UIEventListener>();
+
+            const string closeButtonHierarchy = "Panel/Window Content/Corner X";
+            UIButton closeButton = transform.Find(closeButtonHierarchy).GetComponent<UIButton>();
+
+            closeButton.onClick.Add(new EventDelegate(CloseWindow));
+            cancelButton.onClick = (_) => CloseWindow();
         }
 
         public void Show(string headerText, Action<HierarchyComponentData> pathCallback)
         {
             headerLabel_.text = headerText;
             entryCallback_ = pathCallback;
+
+            levelPathInput_.defaultText = "Enter Component Name";
+            SetLevelPathInput(string.Empty);
         }
 
         private void GenerateComponentNameList()
         {
             buttonList_.Clear();
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
                 HierarchyComponentFile file = new HierarchyComponentFile($"File #{i}", $"component#{i}", null);
-                ComponentEntry entry = ComponentEntry.CreateEntry(file, (_) => { }, (_) => { }, true, Color.cyan);
+                ComponentEntry entry = ComponentEntry.CreateEntry(file, ClickOnEntry, (_) => { }, true, Color.cyan);
+                entry.sortString_ = $"component#{i}";
                 buttonList_.Add(entry);
             }
 
             buttonList_.SortAndUpdateVisibleButtons();
+        }
+
+        public void SetLevelPathInput(string name)
+        {
+            levelPathInput_.Value_ = name;
+        }
+
+        public void ClickOnEntry(UIExButtonContainer.Entry e)
+        {
+            ComponentEntry entry = e as ComponentEntry;
+            SetLevelPathInput(entry.item_.Name);
         }
 
         public class ComponentEntry : UIExButtonContainer.Entry
