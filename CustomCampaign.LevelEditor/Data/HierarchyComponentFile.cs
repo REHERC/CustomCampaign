98aa18928e7ec489ab15abd293b50dc6ec1aa9e7
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-05-12 02:05:22 +0200

    Added type property for HierarchyComponentFile

--- CustomCampaign.LevelEditor/Data/HierarchyComponentFile.cs
+++ CustomCampaign.LevelEditor/Data/HierarchyComponentFile.cs
@@ -1,5 +1,7 @@
 ﻿#pragma warning disable RCS1170
 
+using System;
+
 namespace CustomCampaign.LevelEditor.Data
 {
     public class HierarchyComponentFile : HierarchyComponentData
@@ -9,10 +11,16 @@ namespace CustomCampaign.LevelEditor.Data
         public override string Path => Id.ToLowerInvariant();
 
         public string Id { get; private set; }
+        public Type ComponentType { get; private set; }
 
         public HierarchyComponentFile(string name, string id) : base(name, null)
         {
             Id = id;
+        }
+
+        public void SetComponent(Type component)
+        {
+            ComponentType = component;
         }
     }
 }
